4da48153fd7ebddbfa01eafe11a630fb149d4a5c
Daniel Cruz <d.afc98@gmail.com>
2021-08-08 22:20:20 -0300

    using commands in viewmodel

--- MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,8 +1,36 @@
-﻿using Xamarin.Forms;
+﻿using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : BindableObject
     {
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        int count = 0;
+        string countDisplay = "Click Me!";
+        public ICommand IncreaseCount { get; }
+
+        void OnIncrease(object obj)
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
     }
 }
--- MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows.Input;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MyCoffeeApp.Views
@@ -7,33 +6,9 @@ namespace MyCoffeeApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoffeeEquipmentPage : ContentPage
     {
-        int count = 0;
-        string countDisplay = "Click Me!";
-        public ICommand IncreaseCount { get; }
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set
-            {
-                if (value == countDisplay)
-                    return;
-
-                countDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
-            IncreaseCount = new Command(OnIncrease);
-            BindingContext = this;
-        }
-
-        void OnIncrease(object obj)
-        {
-            count++;
-            CountDisplay = $"You clicked {count} time(s)";
         }
     }
 }
